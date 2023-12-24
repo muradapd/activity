c488d815ed347a36249e8b97e982c6d7d51f8068
Patrick Muradaz <patrickmz27@gmail.com>
2023-12-24 15:33:36 -0500

    Created 2023-12-24.txt

---
+++ update.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /home/muradap/Development/activiy
+CURRENT_DATE=$(date +%Y-%m-%d)
+touch "$CURRENT_DATE.txt"
+git add .
+git commit -m "Created $CURRENT_DATE.txt"
+git push origin main
